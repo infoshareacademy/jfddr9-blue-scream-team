0ccb815c4657ab15839f90bcd50810fc27c1d4b7
aleksandra-jablonska <aleksandra.ola.jablonski@gmail.com>
2023-05-10 10:04:24 +0200

    firebase config

---
+++ src/api/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAzQlyRG7Wub6ZBko6scaBgreSux3glKG0",
+  authDomain: "bluescreamteam-fde79.firebaseapp.com",
+  projectId: "bluescreamteam-fde79",
+  storageBucket: "bluescreamteam-fde79.appspot.com",
+  messagingSenderId: "586360293483",
+  appId: "1:586360293483:web:19375232a6d13fdf9a092e",
+};
