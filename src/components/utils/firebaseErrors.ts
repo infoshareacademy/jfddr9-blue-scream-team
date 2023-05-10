7cf64b75e5bb29651ce23dfe4235ec78ea143002
Aleksandra Jabłońska <125560157+aleksandra-jablonska@users.noreply.github.com>
2023-05-10 11:47:39 +0200

    Olajablonska (#1)
    * firebase config
    
    * changes

---
+++ src/components/utils/firebaseErrors.ts
@@ -0,0 +1,9 @@
+export const firebaseErrors = {
+  "auth/invalid-email": "E-mail is invalid",
+  "auth/weak-password": "Password should be at least 6 characters",
+  "auth/email-already-in-use":
+    "Account registered for this e-mail is already in use",
+  "auth/wrong-password": "Your password is wrong",
+  "auth/user-not-found": "E-mail doesn't exist",
+  "auth/missing-email": "You must provide an email address",
+};
