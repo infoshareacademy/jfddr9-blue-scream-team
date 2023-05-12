e7c6266523b94c989fa6d6eca25a2b5881295caf
Katarzyna Brejna <kb.brejna@gmail.com>
2023-05-12 13:17:41 +0200

    API in preogress

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,9 +1,9 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function Tile() {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
