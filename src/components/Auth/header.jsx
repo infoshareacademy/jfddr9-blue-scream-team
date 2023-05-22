07b5ce364304a879521627a6e3fbdd6a889b1225
Pawel Kurowski <126067451+Prometeosz@users.noreply.github.com>
2023-05-22 14:37:28 +0200

    change styles (#30)

--- src/components/Auth/header.jsx
+++ src/components/Auth/header.jsx
@@ -9,9 +9,10 @@ import {
 } from "firebase/auth";
 import { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import logo from "../../images/logo.png"
-import { clickfunction } from "../../components/Theme"
-import { clickfunction2 } from '../../components/Theme';
+import logo from "../../images/logo.png";
+import { clickfunction } from "../../components/Theme";
+import { clickfunction2 } from "../../components/Theme";
+import { AuthButtons } from "./AuthButtons";
 const HeaderText = styled.div`
   // display: flex;
   // max-width: 1920px;
@@ -22,7 +23,6 @@ const HeaderText = styled.div`
   // align-items: center;
   // font-size: 45px;
 `;
-
 
 export function Header() {
   const navigate = useNavigate();
@@ -48,31 +48,24 @@ export function Header() {
     navigate("/");
   };
   return (
-    
-      
-      
-      <div className="headerbuttons">
-
-      <img className="logo" src= {logo}></img>
-
-      
+    <div className="headerbuttons">
+      <img className="logo" src={logo}></img>
 
       <div className="buttons">
-            <button onClick={() => clickfunction()} className="firstbutton">Dark mode</button>
-            <button onClick={() => clickfunction2()} className="firstbutton">Light mode</button>
+        <button onClick={() => clickfunction()} className="firstbutton">
+          Dark mode
+        </button>
+        <button onClick={() => clickfunction2()} className="firstbutton">
+          Light mode
+        </button>
       </div>
       <div className="buttons">
         {isAuth && (
           <button onClick={handleClick} className="firstbutton">
             Logout
           </button>
-        
         )}
       </div>
-      </div>
-    
-        
-      
-    
+    </div>
   );
 }
