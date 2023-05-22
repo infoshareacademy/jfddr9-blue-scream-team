5af55ce76c71451961069a3b6752350e94fb5b51
ToJurek <tomaszjurkiewicz36@gmail.com>
2023-05-22 14:46:36 +0200

    prettier fix

--- src/pages/background.jsx
+++ src/pages/background.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import telko from "../images/telko.jpg"
+import React from "react";
+import telko from "../images/telko.jpg";
 
 const Background = () => {
   return (
     <div>
-      <img src= {telko} className='background1'></img>
+      <img src={telko} className="background1"></img>
     </div>
   );
 };
