6671ef5d08a94421385e094f6be9c39e1f2aa572
Robert Dabrowski <robertdabrowski2023@gmail.com>
2023-05-22 12:44:44 +0200

    changes

---
+++ src/components/Component.jsx
@@ -0,0 +1,11 @@
+import image from "../images/luggage.jpg"; 
+
+function Component() {
+  return (
+    <div style={{ backgroundImage:`../images/luggage.jpg` }}>
+      Hello World
+    </div>
+  );
+}
+
+export { Component };
