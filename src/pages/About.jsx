4e94f945d67683bcede136bf5eca2e61e867016f
mateuszczap <125034534+mateuszczap@users.noreply.github.com>
2023-05-25 15:12:49 +0200

    About us (#65)
    * add
    
    * add style to about us cards

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,10 +10,16 @@ function About() {
 
   return (
     <div>
-      <h1>About us</h1>
+      <h1 className="about">About us</h1>
       <ul className="personsList">
-        <li>
-          <p> Aleksandra </p>
+        <li className="about-us">
+          <p className="name"> Aleksandra </p>
+          <img
+            src="https://media.licdn.com/dms/image/D4D35AQGZ7Me2rqI1uw/profile-framedphoto-shrink_200_200/0/1681246290333?e=1685624400&v=beta&t=nvJ3L6TPVqBHwE9SkciyKyVyQ9CkdfEs9uu9cqzp_BA"
+            width="100"
+            height="100"
+            className="img-link"
+          ></img>
           <button
             className="firstbutton"
             onClick={() => {
@@ -27,8 +33,14 @@ function About() {
           </button>
         </li>
 
-        <li>
-          <p>Katarzyna</p>
+        <li className="about-us">
+          <p className="name"> Katarzyna </p>
+          <img
+            src="https://media.licdn.com/dms/image/D5635AQGKZ_AFkPDkmA/profile-framedphoto-shrink_200_200/0/1682314414571?e=1685624400&v=beta&t=EHJ4g3qe98fg78LDFA0k3Ya6aubvk8L4OvePTG5J3gE"
+            width="100"
+            height="100"
+            className="img-link"
+          ></img>
           <button
             className="firstbutton"
             onClick={() => {
@@ -42,8 +54,14 @@ function About() {
           </button>
         </li>
 
-        <li>
-          <p>Krzysztof</p>
+        <li className="about-us">
+          <p className="name"> Krzysztof </p>
+          <img
+            src="https://media.licdn.com/dms/image/C4D03AQFUcZ9zU45E_A/profile-displayphoto-shrink_200_200/0/1544175272003?e=1690416000&v=beta&t=VgLjxW2U7HwUfxBTHelmCPUKRM0Q27GbMypWNVsZsAs"
+            width="100"
+            height="100"
+            className="img-link"
+          ></img>
           <button
             className="firstbutton"
             onClick={() => {
@@ -57,8 +75,14 @@ function About() {
           </button>
         </li>
 
-        <li>
-          <p>Mateusz</p>
+        <li className="about-us">
+          <p className="name"> Mateusz </p>
+          <img
+            src="https://media.licdn.com/dms/image/D4D03AQGYwCnzJ-dwxg/profile-displayphoto-shrink_200_200/0/1684408470765?e=1690416000&v=beta&t=Z5ya4mVsywdXK3Wyva6Dy_aXsfP0tFpVu67ZzA90szA"
+            width="100"
+            height="100"
+            className="img-link"
+          ></img>
           <button
             className="firstbutton"
             onClick={() => {
@@ -72,8 +96,14 @@ function About() {
           </button>
         </li>
 
-        <li>
-          <p>Paweł</p>
+        <li className="about-us">
+          <p className="name"> Paweł </p>
+          <img
+            src="https://media.licdn.com/dms/image/D4D03AQEO7UInUc6qvw/profile-displayphoto-shrink_200_200/0/1683018766351?e=1690416000&v=beta&t=SY9zItKqJ6yOIemqxWR61LJJK5xNkacMpSEQGjV601E"
+            width="100"
+            height="100"
+            className="img-link"
+          ></img>
           <button
             className="firstbutton"
             onClick={() => {
@@ -87,8 +117,14 @@ function About() {
           </button>
         </li>
 
-        <li>
-          <p>Robert</p>
+        <li className="about-us">
+          <p className="name"> Robert </p>
+          <img
+            src="https://media.licdn.com/dms/image/D4E35AQHGn49boT3yIQ/profile-framedphoto-shrink_200_200/0/1681941664182?e=1685624400&v=beta&t=UERkpoUhbTMRB-tH72Vimk-TaAQ-zKBRrStY43JgBOo"
+            width="100"
+            height="100"
+            className="img-link"
+          ></img>
           <button
             className="firstbutton"
             onClick={() => {
