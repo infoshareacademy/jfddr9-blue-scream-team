be0abab257fab11ab928547a05f4137d992f0016
Robert Dabrowski <robertdabrowski2023@gmail.com>
2023-05-26 11:16:42 +0200

    links to images

--- src/components/picturegrid.jsx
+++ src/components/picturegrid.jsx
@@ -12,18 +12,42 @@ export const PictureGrid = () => {
   return (
     <div className="row-img">
       <div className="column-img">
-        <img src={fotka1} style={{ width: "100%" }} />
-        <img src={fotka2} style={{ width: "100%" }} />
+        <a href="https://pl.wikipedia.org/wiki/Koloseum">
+          <img title="Colloseum" src={fotka1} style={{ width: "100%" }} />
+        </a>
+        <a href="https://pl.wikipedia.org/wiki/Wie%C5%BCa_Eiffla">
+          <img title="Eiffle Tower" src={fotka2} style={{ width: "100%" }} />
+        </a>
       </div>
       <div className="column-img">
-        <img src={fotka4} style={{ width: "100%" }} />
-        <img src={fotka5} style={{ width: "100%" }} />
-        <img src={fotka8} style={{ width: "100%" }} />
+        <a href="https://pl.wikipedia.org/wiki/Krzywa_Wie%C5%BCa_w_Pizie">
+          <img title="Piza" src={fotka4} style={{ width: "100%" }} />
+        </a>
+        <a href="https://pl.wikipedia.org/wiki/Giza">
+          <img title="Giza" src={fotka5} style={{ width: "100%" }} />
+        </a>
+        <a href="https://en.wikipedia.org/wiki/Triumphal_arch">
+          <img title="Triumphal Arch" src={fotka8} style={{ width: "100%" }} />
+        </a>
       </div>
       <div className="column-img">
-        <img src={fotka7} style={{ width: "100%" }} />
-        <img src={fotka6} style={{ width: "100%" }} />
-        <img src={fotka9} style={{ width: "100%" }} />
+        <a href="https://www.filmweb.pl/film/W%C5%82adca+Pier%C5%9Bcieni%3A+Dru%C5%BCyna+Pier%C5%9Bcienia-2001-1065">
+          <img
+            title="House of Bilbo Baggins"
+            src={fotka7}
+            style={{ width: "100%" }}
+          />
+        </a>
+        <a href="https://pl.wikipedia.org/wiki/Golden_Gate_Bridge">
+          <img title="Golden Gate" src={fotka6} style={{ width: "100%" }} />
+        </a>
+        <a href="https://infoshareacademy.com/">
+          <img
+            title="Infoshare Head Office"
+            src={fotka9}
+            style={{ width: "100%" }}
+          />
+        </a>
       </div>
     </div>
   );
